ed81ac7f6d0b762d1e0877f5f8f125cc2a40171a
Jeff Roberts <jeff.roberts@rmsginc.com>
2020-07-29 19:47:13 -0500

    add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('contact', 'ContactController')->middleware('auth');
-Route::resource('user', 'UserController')->middleware('auth');
+Route::resource('user', 'UserController')->middleware('admin');
 
